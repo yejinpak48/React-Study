da7a20e0739453bd84905e275e6294a07cf629ee
yejinpak48 <yejinpak48@gmail.com>
2022-01-07 23:51:21 +0900

    feat: ExpenseDate 컴포넌트 추가

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const year = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const day = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
