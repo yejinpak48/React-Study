5222aa27e9f5a0bee1e51cc21d4f7313abb8585f
yejinpak48 <yejinpak48@gmail.com>
2022-01-10 21:15:14 +0900

    feat: onClick event handle 추가

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,12 @@
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
-
+  const clickHandler = () => {
+    console.log("버튼 클릭");
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
